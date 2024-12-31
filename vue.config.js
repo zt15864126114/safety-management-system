1886db3417396053d9bfae1fc8bea351510c8969
zhaoteng <1813831021@qq.com>
2024-12-31 10:38:48 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/your-repo-name/'  // 替换成你的仓库名
+        ? '/safety-management-system/'  // 替换成你的仓库名
         : '/',
     devServer: {
         port: 8080,

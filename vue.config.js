138c5deba92d2eb73917e3c827faf717baf687a4
zhaoteng <1813831021@qq.com>
2024-12-31 14:58:29 +0800

    Update vue config and build to docs

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/safety-management-system/'  // 替换成你的仓库名
-        : '/',
+        : '/',outputDir: 'docs',
     devServer: {
         port: 8080,
         open: true
